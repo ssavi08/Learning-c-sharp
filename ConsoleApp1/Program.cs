172d115f045227175713a4515b91bfccdd727c45
ssavi08 <sandro.savi0088@gmail.com>
2024-12-23 20:05:22 +0100

    simple converter from hrk to eur

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+using System.Threading.Channels;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = false;
+
+            Console.WriteLine("Odaberi:");
+            Console.WriteLine("e: EUR -> HRK");
+            Console.WriteLine("h: HRK -> EUR");
+            Console.WriteLine("Za izlazak upiši exit.");
+
+            while (exit == false)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "e":
+                        Console.Write("Unesi eure: ");
+                        double eur = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("HRK: " + eur * 7.55);
+                        break;
+                    case "h":
+                        Console.Write("Unesi kune: ");
+                        double kune = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("HRK: " + kune / 7.55);
+                        break;
+                    case "exit":
+                        exit = true;
+                        break;
+                }
+
+            }
+            
+        }
+    }
+}
